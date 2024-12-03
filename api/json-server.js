69b67f4f19bef78315a995087bc783f7ecf3c178
moe-jamal <moe.jamal.360@gmail.com>
2024-12-03 18:35:09 +0200

    fix issue

--- api/json-server.js
+++ api/json-server.js
@@ -1,6 +1,8 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import path from "path";
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Ensure this path is correct
+const router = jsonServer.router(path.join(__dirname, "../db.json")); // Ensure the correct path
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -9,3 +11,5 @@ server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+export default server;
